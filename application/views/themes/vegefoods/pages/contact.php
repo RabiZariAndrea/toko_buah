1cf3f69477e3f556c1868a9fcbe8710d1d203899
RabiZariAndrea <rabizari78@gmail.com>
2024-01-03 22:13:31 +0700

    first commit

---
+++ application/views/themes/vegefoods/pages/contact.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="hero-wrap hero-bread" style="background-image: url('<?php echo get_theme_uri('images/bg_4.jpg'); ?>');">
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+            <p class="breadcrumbs"><span class="mr-2"><?php echo anchor(base_url(), 'Home'); ?></span> <span>Hubungi Kami</span></p>
+          <h1 class="mb-0 bread">Hubungi Kami</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="ftco-section contact-section bg-light">
+    <div class="container">
+        <div class="row d-flex mb-5 contact-info">
+        <div class="w-100"></div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Alamat</span> <?php echo get_settings('store_address'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>No. HP</span> <?php echo get_settings('store_phone_number'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Email:</span> <?php echo get_settings('store_email'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Website</span> </p>
+            </div>
+        </div>
+      </div>
+      <div class="row block-9">
+        <div class="col-md-6 order-md-last d-flex">
+          <form action="<?php echo site_url('pages/send_message'); ?>" class="bg-white p-5 contact-form" method="POST">
+          <?php if ($flash) : ?>
+          <div class="text-success text-center" style="margin-bottom: 25px;"><?php echo $flash; ?></div>
+          <?php endif; ?>
+
+            <div class="form-group">
+              <input type="text" name="name" class="form-control" value="<?php echo set_value('name', (is_login() ? get_user_name() : '')); ?>" placeholder="Nama" required>
+              <?php echo form_error('name'); ?>
+            </div>
+            <div class="form-group">
+              <input type="email" name="email" class="form-control" value="<?php echo set_value('email', (is_login() ? $user->email : '')); ?>" placeholder="Email" required>
+              <?php echo form_error('email'); ?>
+            </div>
+            <div class="form-group">
+              <input type="text" name="subject" class="form-control" value="<?php echo set_value('subject'); ?>" placeholder="Subjek pesan" required>
+              <?php echo form_error('subject'); ?>
+            </div>
+            <div class="form-group">
+              <textarea name="message" id="" cols="30" rows="7" class="form-control" placeholder="Pesan" required><?php echo set_value('message'); ?></textarea>
+              <?php echo form_error('message'); ?>
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Kirim Pesan" class="btn btn-primary py-3 px-5">
+            </div>
+          </form>
+        
+        </div>
+
+        <div class="col-md-6 d-flex">
+            <div style="width: 100%">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6861295939248!2d106.96151689999999!3d-6.3049056000000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699262cd011309%3A0x6fdcf4616bf494ee!2sJl.%20SDN%20Jatirasa%201%20No.98%2C%20RT.004%2FRW.001%2C%20Jatirasa%2C%20Kec.%20Jatiasih%2C%20Kota%20Bks%2C%20Jawa%20Barat%2017424!5e0!3m2!1sid!2sid!4v1693248967518!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
